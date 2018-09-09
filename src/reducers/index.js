966ed7b5800ebf3634d590fdcbd51e050b778646
Anatoliy Ark <anatoliy.arkove@gmail.com>
2018-09-09 06:23:20 +0300

    remoing redundent files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
-import postReducer from './postReducer';
 import elevetorReducer from './elevetorReducer';
 
 export default combineReducers ({
-  posts:postReducer,
   elevetorReducer:elevetorReducer
 });
 
