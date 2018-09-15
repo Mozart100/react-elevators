5e6af666b590dcc183e1fd8ba41d0e2661b5721a
Mozart100 <anatoliy.arkove@gmail.com>
2018-09-15 09:55:06 +0300

    elevaror 2

--- src/components/elevator2.js
+++ src/components/elevator2.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import elevatorImage from './Images/elv.png';
@@ -14,7 +15,14 @@ import './style/elevator-style.css'
 //    to { top:${to}px }
 // `;
 const ElevatorMoverStyled = (from, to) => keyframes`
-   0% { top:${from}px }
+   0% { top:${from}px; }
+   /* 0% { top:${from}px; z-index:0 }
+   10% { z-index:1 }
+   20% { z-index:2 }
+   30% { z-index:3 } */
+
+/* 50% { top:250px; } */
+
     /* 99% { top:${to}px }  */
    100% { top:${to}px }
 `;
@@ -30,6 +38,7 @@ const ImgStyled = styled.img`
   animation: ${props => ElevatorMoverStyled(props.currentTop, props.designatedTop)} ${props => props.animationTime}s linear;
   /* animation: ${props => ElevatorMoverStyled(props.currentTop, props.designatedTop)} 2s linear; */
   animation-fill-mode:forwards;
+  animation-iteration-count:1;
   /* animation: ${props => ElevatorMoverStyled(props.currentTop, props.designatedTop)} infinite 5s linear; */
 `;
 /* animation: ${ElevatorMoverStyled}  ${props.animationTimeInSeconds}s linear; */
@@ -51,10 +60,12 @@ class Elevator2 extends Component {
             numberFloorPressed: 1,
 
             totalHeight: this.props.amountOfFloors * this.props.componentHeight,
-            currentTop: (this.props.amountOfFloors * this.props.componentHeight) - this.props.componentHeight + 2,
+            currentTop: (this.props.amountOfFloors * this.props.componentHeight) - this.props.componentHeight,
             designatedTop: (this.props.amountOfFloors * this.props.componentHeight) - this.props.componentHeight,
 
         }
+
+        this.currentFloor = 1;
 
         // console.log('componentHeight = ',this.props.componentHeight);
 
@@ -127,29 +138,76 @@ class Elevator2 extends Component {
     }
 
     componentDidMount = () => {
-        const element = document.getElementById('anatoliy_component');
-        element.addEventListener('animationend', () => {
-            console.log('animation finished');
-            const { currentTop, designatedTop } = this.state;
 
-            if (currentTop !== designatedTop) {
-                this.setState({ currentTop: designatedTop });
-            }
-          // Do anything here like remove the node when animation completes or something else!
+        this.timer = setInterval(() => this.TimerAction(), 10);
+
+        // thie.elevator = 
+
+
+        const element = document.getElementById('anatoliy_component');
+
+
+        element.addEventListener('animationiteration', () => {
+            console.log('sssssssssssss')
         });
-      };
+        // element.addEventListener('animationend', (e) => {
+
+
+
+
+        // element.addEventListener('animationstart', (e) => {
+        //     console.log('animationstart');
+        // });
+
+        // element.addEventListener('animationend', (e) => {
+        //     console.log('animation finished');
+
+        //     // console.log( ReactDOM.findDOMNode(this.refs.elevator));
+        //     const { currentTop, designatedTop } = this.state;
+        //     // const elem = this.elevator;
+
+        //     if (currentTop !== designatedTop) {
+        //         this.setState({ currentTop: designatedTop });
+        //     }
+        //     // Do anything here like remove the node when animation completes or something else!
+        // });
+
+        // element.addEventListener('animationiteration', (e) => {
+        // element.addEventListener('webkitAnimationIteration', (e) => {
+        //     element.addEventListener('webkitAnimationIteration', function () {
+        //     console.log('animationiteration finished');
+        //     debugger;
+
+        // });
+
+
+    };
+
+    TimerAction = () => {
+
+        const { currentTop, designatedTop } = this.state;
+
+        if (currentTop === designatedTop) {
+            return;
+        }
+
+        // (this.props.amountOfFloors * this.props.componentHeight) - this.props.componentHeight,
+
+
+        // this.setState({ currentTop: designatedTop });
+    }
 
     render() {
         const { currentTop, designatedTop } = this.state;
         const animationTime = Math.abs(designatedTop - currentTop) / 50 * 0.5;
-        console.log('currentTop=', currentTop);
-        console.log('designatedTop=', designatedTop);
-        console.log('animationTime=', animationTime);
+        // console.log('currentTop=', currentTop);
+        // console.log('designatedTop=', designatedTop);
+        // console.log('animationTime=', animationTime);
         return (
             // <ImgStyled src={elevatorImage} currentTop={450}
             //     designatedTop={200} animationTime={2}></ImgStyled>
 
-
+            // ref={elem=>this.elevator = elem}
             <ImgStyled src={elevatorImage} currentTop={currentTop} id="anatoliy_component"
                 designatedTop={designatedTop} animationTime={animationTime}></ImgStyled>
 
@@ -162,7 +220,12 @@ class Elevator2 extends Component {
     componentDidUpdate(prevProps, prevState, prevContext) {
         // console.log('componentDidUpdate(prevProps, prevState, prevContext)')
 
-        const { currentTop, designatedTop } = this.state;
+        // const element = document.getElementById('anatoliy_component');
+        // console.log( 'ref = ',element);
+        // console.log( 'ref = ',element.style);
+        // console.log( 'ref = ',ReactDOM.findDOMNode(this.refs.elevator));
+
+        // const { currentTop, designatedTop } = this.state;
 
         // if (currentTop !== designatedTop) {
         //     this.setState({ currentTop: designatedTop });
